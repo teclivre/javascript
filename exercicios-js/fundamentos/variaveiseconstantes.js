af8078bef9ad9da759a2d1af84d0406b466a3458
teclivre <contato@portalsjcampos.com>
2020-11-05 19:49:48 -0300

    Exercicios sobre variaveis

---
+++ exercicios-js/fundamentos/variaveiseconstantes.js
@@ -0,0 +1,17 @@
+var a = 3 // Usando o var para declarar variaveis
+let b = 4 // Usando o let para declarar variaveis
+
+
+
+console.log(a)
+console.log(b)
+
+a = 300
+b = 400
+
+console.log(a+b)
+
+const c = 15
+// c = 5 
+console.log(c)
+
